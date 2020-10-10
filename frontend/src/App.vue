20f7a4458e45344e054d62bdf81f5fd3d9ad6289
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-10 03:12:39 +0200

    added google font

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,8 +18,9 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Sora:wght@100;300;500;700&display=swap');
 * {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Sora', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
